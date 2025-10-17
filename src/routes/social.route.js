37f1a997c809c3db0873c2b907be1499d46efa3c
OLuwatofunmi <tofunmibolaji13@gmail.com>
2025-10-17 16:18:44 +0100

    update

--- src/routes/social.route.js
+++ src/routes/social.route.js
@@ -4,10 +4,5 @@ const authenticateToken = require("../middleware/authToken.middleware");
 
 const router = express.Router();
 
-router.post("/analytics", authenticateToken, socialController.getSocialAnalytics);
-router.get("/instagram/auth", socialController.startInstagramAuth);
-router.post("/instagram/callback", socialController.instagramCallback);
-router.post("/youtube/analytics", authenticateToken, socialController.getYouTubeAnalyticsByUsername);
-router.get("/youtube/channel", authenticateToken, socialController.getYouTubeChannelInfo);
 
 module.exports = router;
